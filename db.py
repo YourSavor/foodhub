82ca64fcefcf3ea38139f59e318e76e6b52eeae1
ChanMor <morelos.franzchristian@gmail.com>
2024-05-02 10:07:26 +0800

    Initial commit

---
+++ db.py
@@ -0,0 +1,38 @@
+import os
+
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv()
+
+def establish_connection():
+    connection = psycopg2.connect(                                                  
+        user = os.getenv("DATABASE_USERNAME"),                                      
+        password = os.getenv("DATABASE_PASSWORD"),                                  
+        host = os.getenv("DATABASE_IP"),                                            
+        port = os.getenv("DATABASE_PORT"),                                          
+        database = os.getenv("DATABASE_NAME")                                       
+    )    
+
+    cursor = connection.cursor()
+    return connection, cursor
+
+def close_connection(connection, cursor):
+    connection.close()
+    cursor.close()
+
+def sample_query():
+    connection, cursor = establish_connection()
+
+    cursor.execute("INSERT INTO users (username, hashed_password, first_name, middle_name, last_name) VALUES ('test', 'test', 'test', 'test', 'test');")
+    connection.commit()
+    print('Succesfully Inserted Test User')
+
+    cursor.execute("SELECT * FROM users;")
+    records = cursor.fetchone()
+    connection.commit()
+    print(f'User: {records}')
+
+    close_connection(connection, cursor)
+
+sample_query()
